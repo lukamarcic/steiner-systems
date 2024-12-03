de836bbdb13859cb2b0d7c7e13d58be84cce483e
Luka <marcicluke@gmail.com>
2024-12-03 13:07:49 +0100

    Initial commit

---
+++ lakefile.lean
@@ -0,0 +1,16 @@
+import Lake
+open Lake DSL
+
+package "steiner-systems" where
+  version := v!"0.1.0"
+  keywords := #["math"]
+  leanOptions := #[
+    ⟨`pp.unicode.fun, true⟩, -- pretty-prints `fun a ↦ b`
+    ⟨`autoImplicit, false⟩
+  ]
+
+require "leanprover-community" / "mathlib"
+
+@[default_target]
+lean_lib «SteinerSystems» where
+  -- add any library configuration options here
